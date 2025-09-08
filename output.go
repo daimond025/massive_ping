b5d1a09686a4947535bbbe549d4acb0cbf8059e5
daimond025 <daimond025@yandex.ru>
2025-09-08 21:33:33 +0300

    Correct logs

--- output.go
+++ output.go
@@ -2,8 +2,8 @@ package ping
 
 import "fmt"
 
-func out_std(p *Pinger) {
-	size = uint(len(p.dataload))
+func Out_std(p *Pinger) {
+	size := uint(len(p.dataload))
 
 	for adds, stats := range p.history {
 
@@ -13,7 +13,5 @@ func out_std(p *Pinger) {
 		out := fmt.Sprintf("Host: %s; recieve bytes %v,  send - %v, receive - %v; statistics min -  %s, max - %s, mean  - %s",
 			adds, size, stats.send, stats.received, stats.getBest(), stats.getWorst(), stats.getMean())
 		fmt.Println(out)
-
 	}
-
 }
